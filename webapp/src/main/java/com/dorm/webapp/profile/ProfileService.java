92e14d16aaf05aaac0fdf845a0f34dffc506592e
Jakub Kwiatkowski <jkwiatko94@gmail.com>
2020-01-13 16:46:48 +0100

    Clean project unused files

--- webapp/src/main/java/com/dorm/webapp/profile/ProfileService.java
+++ webapp/src/main/java/com/dorm/webapp/profile/ProfileService.java
@@ -1,29 +1,22 @@
 package com.dorm.webapp.profile;
 
 import com.dorm.webapp.data.entity.User;
-import com.dorm.webapp.data.repo.UserRepository;
 import com.dorm.webapp.data.service.UserService;
-import com.dorm.webapp.util.image.ImageStorage;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.text.ParseException;
 
 @Service
 public class ProfileService {
 
     private final UserService userService;
     private final ModelMapper modelMapper;
-    private final ImageStorage imageStorage;
 
     public ProfileService(
             ModelMapper modelMapper,
-            UserService userService,
-            ImageStorage imageStorage
+            UserService userService
     ) {
         this.modelMapper = modelMapper;
         this.userService = userService;
-        this.imageStorage = imageStorage;
     }
 
     public void updateUserDetails(ProfileDTO profile) {
