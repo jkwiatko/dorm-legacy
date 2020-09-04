5bedbfb18e06c373cf51b2591d4970161b77594e
Jakub Kwiatkowski <jkwiatko94@gmail.com>
2020-09-04 12:28:52 +0200

    Fix typo and add ionic cli to dev dependencies

--- frontend/src/app/shared-module/pipes/room-number.pipe.ts
+++ frontend/src/app/shared-module/pipes/room-number.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RoomNumberPipe implements PipeTransform {
 
   transform(numberOfRooms: number): string {
-      return numberOfRooms > 4 ? numberOfRooms + ' pokoji' : numberOfRooms > 1 ? numberOfRooms + ' pokoje' : '1 pokój';
+      return numberOfRooms > 4 ? numberOfRooms + ' pokoi' : numberOfRooms > 1 ? numberOfRooms + ' pokoje' : '1 pokój';
   }
 
 }
